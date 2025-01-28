a4980fa644dfb18c9694d3230b6a3f0bc26911bb
Gideon Edoghotu <edoghotugiddy@gmail.com>
2025-01-28 09:53:20 +0100

    updated

--- Core/AccountManagement.Domain/IdentityHub/User.cs
+++ Core/AccountManagement.Domain/IdentityHub/User.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AccountManagement.Domain.IdentityHub
+﻿namespace AccountManagement.Domain.IdentityHub
 {
     public class User
     {
-        
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
