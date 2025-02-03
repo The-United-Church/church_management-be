3c287dca4b17a31f8b50c93f3b091550fbca692e
Gideon Edoghotu <edoghotugiddy@gmail.com>
2025-02-03 19:32:52 +0100

    added the database context for the user

---
+++ AccountManagement.Infrastructure/DatabaseContext/CHURCHDatabaseContext.cs
@@ -0,0 +1,46 @@
+﻿using AccountManagement.Application.Utils;
+using AccountManagement.Domain.IdentityHub;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.Infrastructure.DatabaseContext
+{
+    public class CHURCHDatabaseContext : DbContext
+    {
+        public CHURCHDatabaseContext(DbContextOptions<CHURCHDatabaseContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CHURCHDatabaseContext).Assembly);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable(ApplicationConstants.CONNECTION_STRING),
+                    sqlServerOptionsAction: sqlOptions => sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorNumbersToAdd: null
+                        ));
+            }
+        }
+
+        // User Management DbSet Entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
---
+++ Core/AccountManagement.Application/Utils/ApplicationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagement.Application.Utils
+{
+    public static class ApplicationConstants
+    {
+        public static readonly string CONNECTION_STRING = "MY_APP_CONNECTION_STRING";
+    }
+}
